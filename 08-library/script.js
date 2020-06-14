e8ecda7466485848e496698b52e51cad6489ff8d
Aidan Waugh <mm3w5smjhic3@oo-mail.net>
2020-06-14 09:51:54 -0700

    delete button functional for tr and libraryArray. implemented changeReadState for existing books

--- 08-library/script.js
+++ 08-library/script.js
@@ -13,30 +13,42 @@ function addBook() {
   const pages = document.querySelector("#book-pages").value;
   const checkbox = document.querySelector("#book-state").checked;
 
-  //if readState is true, print new item with 'checked' else leave empty
+  //set read state of book
   if (checkbox == true) {
     readState = "checked";
   } else {
     readState = "";
   }
 
+  //form validation
   if (title == "" || author == "" || pages == "") {
-    console.log("wrong");
+    alert("add correct fields");
   } else {
     const newTitle = new Book(title, author, pages, readState);
+
     myLibraryArray.push(newTitle);
     console.table(myLibraryArray);
+    render(myLibraryArray);
+    clearFormFields();
   }
 }
 
-function deleteBook(book) {
-  // if (book.classList.contains("delete")) {
-  // book.parentElement.parentElement.remove();
-  // }
-  let row = book.parentNode.parentNode;
-  console.log(row.rowIndex);
-  myLibraryArray.splice(row.rowIndex - 1, 1);
-  row.remove();
+function deleteBook(btn) {
+  let tableRow = btn.parentNode.parentNode; //btn>td>tr
+  myLibraryArray.splice(tableRow.rowIndex - 1, 1); //tr of <th> is rowIndex 0, then start at 1
+  tableRow.remove();
+  console.table(myLibraryArray);
+}
+
+function changeReadState(checkbox) {
+  let tableRow = checkbox.parentNode.parentNode;
+  if (myLibraryArray[tableRow.rowIndex - 1].readState == "") {
+    myLibraryArray[tableRow.rowIndex - 1].readState = "checked";
+    console.log(`${myLibraryArray[tableRow.rowIndex - 1].title} readState changed to checked/read`);
+  } else {
+    myLibraryArray[tableRow.rowIndex - 1].readState = "";
+    console.log(`${myLibraryArray[tableRow.rowIndex - 1].title} readState changed to ""/unread`);
+  }
 }
 
 function clearFormFields() {
@@ -54,7 +66,7 @@ function render(library) {
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.pages}</td>
-    <td><input type="checkbox" ${book.readState}/></td?
+    <td><input type="checkbox" ${book.readState}/></td>
     <td><button class="delete-btn delete">X</button></td>
     `;
     bookList.appendChild(newRow);
@@ -63,17 +75,22 @@ function render(library) {
 
 document.querySelector("#submit-btn").addEventListener("click", (e) => {
   e.preventDefault(); //prevent actual submit
+
   addBook();
-  render(myLibraryArray);
-  clearFormFields();
 });
 
 document.querySelector("#book-table").addEventListener("click", (e) => {
-  // Remove book from UI
-  deleteBook(e.target);
-  console.table(myLibraryArray);
+  var clickedItem = e.target;
+  if (clickedItem.tagName == "INPUT") {
+    changeReadState(e.target);
+  }
+  if (clickedItem.tagName == "BUTTON") {
+    deleteBook(e.target);
+  }
 });
-const sampleBook = new Book("Harry Potter", "JK", "200", "Read");
+
+const sampleBook = new Book("Harry Potter", "JK", "200", "");
 myLibraryArray.push(sampleBook);
 
-render(myLibraryArray);
+document.addEventListener("DOMContentLoaded", render(myLibraryArray));
+console.table(myLibraryArray);
