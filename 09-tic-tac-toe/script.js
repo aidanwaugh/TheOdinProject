bdfaed04afb9bc4ac033b2b1ee5c2be8deec75d8
Aidan Waugh <mm3w5smjhic3@oo-mail.net>
2020-06-16 15:31:47 -0700

    function dumb ai opponent.

--- 09-tic-tac-toe/script.js
+++ 09-tic-tac-toe/script.js
@@ -11,6 +11,7 @@ function player(playerName, symbol) {
 
 const gameBoard = (() => {
   let gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+  let gameBoardAiOptionArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]; //options for ai
   const player1 = player("player1", "x", "xClass");
   const player2 = player("player2", "o", "oClass");
   let currentPlayer = player1;
@@ -20,18 +21,24 @@ const gameBoard = (() => {
   const btnPlayerVsAi = document.querySelector("#btn-ai");
   let winningCell = ["", "", ""];
   let playGame = false; //lets click and hover TODO: set to false
+  let versusAi = false;
 
   const pickOpponent = () => {
-    console.log("pick opponent run");
+    // console.log("pick opponent run");
     btnPlayerVsPlayer.style.display = "inline-block";
     btnPlayerVsAi.style.display = "inline-block";
     btnPlayerVsPlayer.addEventListener("click", startGame);
+    btnPlayerVsAi.addEventListener("click", () => {
+      versusAi = true;
+      startGame();
+    });
   };
 
   const startGame = () => {
-    console.log("start playing! reset");
+    // console.log("start playing! reset. vs AI=" + versusAi);
     currentPlayer = player1;
     gameBoardArray = ["", "", "", "", "", "", "", "", ""];
+    gameBoardAiOptionArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     playGame = true;
     document.querySelector(".game-board").style.display = "grid";
     msg.innerHTML = "";
@@ -52,7 +59,7 @@ const gameBoard = (() => {
   const hoverPlayerMarkShow = (e) => {
     if (playGame) {
       if (e.target.classList.contains("xClass") || e.target.classList.contains("oClass")) {
-        console.log(`Cell is not empty. No hover effect can be added.`);
+        // console.log(`Cell is not empty. No hover effect can be added.`);
       } else {
         e.target.classList.add("hover-cell");
         e.target.innerHTML = currentPlayer.symbol;
@@ -75,17 +82,39 @@ const gameBoard = (() => {
       cell.classList.remove("hover-cell");
       cell.classList.add(currentPlayer.symbolClass);
       gameBoardArray[cellIndex] = currentPlayer.symbol;
+      gameBoardAiOptionArray[cellIndex] = null;
+      // console.log(gameBoardArray);
       checkWin(currentPlayer);
       switchTurn();
     }
   };
 
   const switchTurn = () => {
+    //ai turn
     currentPlayer == player1 ? (currentPlayer = player2) : (currentPlayer = player1);
+    if (versusAi == true && currentPlayer == player2) {
+      let currentAiOptions = gameBoardAiOptionArray.filter((string) => string != null);
+      // console.log("AI options before" + currentAiOptions);
+      let cellAiIndex = Math.floor(Math.random() * (currentAiOptions.length - 1)) + 0;
+      // debugger;
+      console.log(cellAiIndex + " " + currentAiOptions[cellAiIndex]);
+      //place cell index inside option array
+      let cellAiMark = document.getElementById("cell-" + currentAiOptions[cellAiIndex]);
+      cellAiMark.innerHTML = player2.symbol;
+      cellAiMark.classList.add(player2.symbolClass);
+      // console.log(gameBoardAiOptionArray);
+      gameBoardAiOptionArray[currentAiOptions[cellAiIndex]] = null; //FIXME:
+      gameBoardArray[currentAiOptions[cellAiIndex]] = player2.symbol;
+      // console.log("AI options after" + gameBoardAiOptionArray + " removed " + cellAiIndex);
+      console.log(currentPlayer.playerName);
+      checkWin(player2);
+      currentPlayer = player1;
+    }
     return currentPlayer;
   };
 
   const checkWin = (currentPlayer) => {
+    console.log("checking win " + currentPlayer.playerName + " " + gameBoardArray);
     const winningCombos = [
       ["0", "1", "2"],
       ["3", "4", "5"],
@@ -106,7 +135,7 @@ const gameBoard = (() => {
         winningCell[0] = winningCombos[i][0];
         winningCell[1] = winningCombos[i][1];
         winningCell[2] = winningCombos[i][2];
-        console.log(playGame);
+        // console.log(playGame);
         return [winningCell[0], winningCell[1], winningCell[2], endGameWinner(currentPlayer)];
       }
     }
@@ -120,7 +149,7 @@ const gameBoard = (() => {
   const endGameWinner = (currentPlayer) => {
     //if the array content has the current player class, set background of each cell light grey
     playGame = false;
-
+    versusAi = false;
     document.getElementById("cell-" + winningCell[0]).classList.add("highlight-cell");
     document.getElementById("cell-" + winningCell[1]).classList.add("highlight-cell");
     document.getElementById("cell-" + winningCell[2]).classList.add("highlight-cell");
@@ -133,6 +162,7 @@ const gameBoard = (() => {
 
   const endGameDraw = () => {
     playGame = false;
+    versusAi = false;
     msg.innerHTML = "Game over! It's a draw. Play again?";
     btnPlayerVsPlayer.style.display = "inline-block";
     btnPlayerVsAi.style.display = "inline-block";
