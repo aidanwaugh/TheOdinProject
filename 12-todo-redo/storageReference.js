2af91d7d9c3efda1be75dc28a96d8bc14e42c589
Aidan Waugh <mm3w5smjhic3@oo-mail.net>
2020-07-01 16:03:52 -0700

    new todo, starting from data array. initial render list works

---
+++ 12-todo-redo/storageReference.js
@@ -0,0 +1,19 @@
+/* 
+3 coloumns - on page load, render each column with an empty list
+*/
+let dataArray = [
+  {
+    column: 'col-0',
+    columnName: 'GTD',
+    columnLists: [
+      {
+        listName: '',
+        listIndex: '0',
+        listId: '00',
+        listTasks: [{ taskIndex: '', taskName: '', taskId: '', taskTag: '', taskPriority: '', taskDeadline: '', taskCompleted: '' }],
+      },
+    ],
+  },
+  { col1 },
+  { col2 },
+];
